fb81e7e2268b25ada1cc746dd6b9294e76c4701c
Trisha-sau <moul5335i@gmail.com>
2020-02-06 22:17:54 +0530

    first commit

---
+++ src/test/java/mvnfirst/AppTest.java
@@ -0,0 +1,20 @@
+package mvnfirst;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
